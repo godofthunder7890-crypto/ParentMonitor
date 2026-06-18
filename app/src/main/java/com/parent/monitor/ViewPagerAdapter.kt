2d5ba0710c3ddaddf9ad9d65858663a84a5894a4
godofthunder7890-crypto <godofthunder7890@gmail.com>
2026-06-18 16:23:48 +0530

    Feat: 6 tabs - Dashboard, Live, Data, Control, Notifs, Settings

--- app/src/main/java/com/parent/monitor/ViewPagerAdapter.kt
+++ app/src/main/java/com/parent/monitor/ViewPagerAdapter.kt
@@ -4,21 +4,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(activity: FragmentActivity) :
-    FragmentStateAdapter(activity) {
+class ViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
 
-    val notifFragment = NotificationsFragment()
-    val controlFragment = ControlFragment()
-    val settingsFragment = SettingsFragment()
+    val dashboardFragment   = DashboardFragment()
+    val liveFragment        = LiveFragment()
+    val dataFragment        = DataFragment()
+    val controlFragment     = ControlFragment()
+    val notifFragment       = NotificationsFragment()
+    val settingsFragment    = SettingsFragment()
 
-    override fun getItemCount() = 3
+    override fun getItemCount() = 6
 
-    override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> notifFragment
-            1 -> controlFragment
-            2 -> settingsFragment
-            else -> notifFragment
-        }
+    override fun createFragment(position: Int): Fragment = when (position) {
+        0 -> dashboardFragment
+        1 -> liveFragment
+        2 -> dataFragment
+        3 -> controlFragment
+        4 -> notifFragment
+        else -> settingsFragment
     }
-    }
+}
